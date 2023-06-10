98c87148ec51773a105006bc920dca7f4e209a86
Amey Chavan <ameyc96@gmail.com>
2023-06-10 20:56:12 +0530

    Added initial plan to design terminal UI

---
+++ runner/tui.go
@@ -0,0 +1,20 @@
+package runner
+
+
+/*
+Terminal UI Layout Plan:
+
+|-------------Left Half (Input Controls)------------|--Right Half---|
+
+|-------------------------------------------------------------------|
+|Image To Search: [Text Input]      				|	Search		|
+|                                   				|	Results		|
+|Filters:                           				|	To be		|
+|Stars: [Numeric Input]             				|	Shown		|
+|Automated ?: [Both/Yes/No dropdown]				|	Here		|
+|Official ?: [Both/Yes/No dropdown] 				|           	|
+|                                   				|           	|
+|Result Limit: [Dropdown of 'Default' & 1 to 100]	|           	|
+|									[Search Button]	|           	|
+|-------------------------------------------------------------------|
+*/
